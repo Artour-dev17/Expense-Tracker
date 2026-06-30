cc3eb02f20ce1dcddd04998a14304c2882215094
Artour <47330845+Artour-dev17@users.noreply.github.com>
2026-06-30 17:27:03 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,6 @@ import type { Expense } from "./types/expense";
 
 function App() {
 
-  const handleCountExpenses = async (
-      expense: Expense
-  ) => {
-
-    }
-
-  return (
-      <div className="app">
-          <h1>Expense Tracker</h1>
-
-          <ExpenseForm onAddExpense={handleCountExpenses} />
-
-
-      </div>
-
   )
 }
 
